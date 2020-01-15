97defb7e3d62a57bdb97b1132824501907072f72
Donny Winston <dwinston@alum.mit.edu>
2020-01-15 11:40:10 -0500

    Add reqs and setup file

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+module_dir = Path(__file__).resolve().parent
+
+with open(module_dir / "README.md") as f:
+    long_description = f.read()
+
+setup(
+    name="osmthedistance",
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
+    description="Distance-goal routing using OpenStreetMap data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dwinston/osmthedistance",
+    author="Donny Winston",
+    author_email="dwinston@alum.mit.edu",
+    license="AGPL",
+    packages=find_packages(),
+    install_requires=[
+        "lxml",
+        "pymongo>",
+        "tqdm",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+    ],
+)

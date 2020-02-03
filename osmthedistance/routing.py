e82fc43727a61b99964f86a4230822496a9ee289
Donny Winston <dwinston@alum.mit.edu>
2020-02-03 17:12:59 -0500

    Add logic for turn determination

--- osmthedistance/routing.py
+++ osmthedistance/routing.py
@@ -3,13 +3,33 @@ from collections import deque
 from haversine import haversine, Unit
 import networkx as nx
 
+from osmthedistance.util import triplewise, surface_turn_angle, pairwise
+
 
 class RouteGraph:
-    def __init__(self, vertex_docs, edge_docs):
+    def __init__(self, vertex_docs, edge_docs, goal_distance, waypoints,
+                 goal_tolerance=0.2, max_turns=10, turn_angle=60, turn_radius=15.24):
+        """
+        Construct routes that follow waypoints and that meet the goal distance within tolerance and max_turns.
+
+        Args:
+            goal_distance: goal distance, in miles.
+            waypoints: list of dicts with node id as "id" value and optional distance as "d" value.
+                The "d" values for the first and last points represent distances from real-life origin
+                and destination points to available nodes in the graph.
+            goal_tolerance: fractional tolerance for goal distance. Default is 0.2, i.e. +/- 20% of distance.
+            max_turns: the maximum number of turns a satisfying route may include.
+            turn_angle: in degrees. Default is +/- 60 degrees (i.e. left or right) relative to previous heading.
+            turn_radius: in meters. Default is 15.24m, i.e. 50ft.
+        """
+        if not all(p['id'] in self.G for p in waypoints):
+            raise Exception("Waypoint ids are not node ids.")
+        if len(waypoints) < 2:
+            raise Exception("Need at least two waypoints.")
 
         self.G = nx.Graph()
         self.G.add_nodes_from([
-            (d["_id"], {"coords": d["loc"]["coordinates"]})
+            (d["_id"], {"coords": d["loc"]["coordinates"][::-1]})  # switch from (lon, lat) to (lat, lon)
             for d in vertex_docs
         ])
         self.G.add_edges_from([
@@ -17,67 +37,107 @@ class RouteGraph:
             for d in edge_docs
         ])
 
-    def neighbors(self, me):
-        return [{"id": n, "coords": self.G.nodes[n]['coords']} for n in self.G.neighbors(me)]
+        added_distance = waypoints[0].get("d", 0) + waypoints[-1].get("d", 0)
+        self.min_distance = (goal_distance - added_distance) * (1 - goal_tolerance)
+        self.max_distance = (goal_distance - added_distance) * (1 + goal_tolerance)
+        self.waypoint_ids = [w["id"] for w in waypoints]
 
-    def routes(self, goal_distance, waypoints, tolerance=0.2, max_turns=10, turn_angle=60, turn_radius=15.24):
+        self.goal_distance = goal_distance
+        self.waypoints = waypoints
+        self.goal_tolerance = goal_tolerance
+        self.max_turns = max_turns
+        self.turn_angle = turn_angle
+        self.turn_radius = turn_radius
+
+    def neighbors(self, me):
+        return [n for n in self.G.neighbors(me)]
+
+    def lat_lon(self, me):
+        return self.G.nodes[me]['coords']
+
+    def routes(self):
         """
         Get routes that follow waypoints and that meet the goal distance within tolerance and max_turns.
-
-        Args:
-            goal_distance: goal distance, in miles.
-            waypoints: list of dicts with node id as "id" value and optional distance as "d" value.
-                The "d" values for the first and last points represent distances from real-life origin
-                and destination points to available nodes in the graph.
-            tolerance: fractional tolerance for goal distance. Default is 0.2, i.e. +/- 20% of distance.
-            max_turns: the maximum number of turns a satisfying route may include.
-            turn_angle: in degrees. Default is +/- 60 degrees (i.e. left or right) relative to previous heading.
-            turn_radius: in meters. Default is 15.24m, i.e. 50ft.
 
         Returns:
             list: list of Route objects that satisfy the constraints
         """
-        if not all(p['id'] in self.G for p in waypoints):
-            raise Exception("Waypoint ids are not node ids.")
-        if len(waypoints) < 2:
-            raise Exception("Need at least two waypoints.")
 
         # Goals:
         # - Hit waypoints in order. Prune if hit later waypoint first.
         # - Hit distance within tolerance. Prune if distance > (goal_distance + tolerance).
         # - At most max_turns turns. Prune if n_turns > max_turns.
 
-        added_distance = waypoints[0].get("d", 0) + waypoints[-1].get("d", 0)
-        min_distance = (goal_distance - added_distance) * (1 - tolerance)
-        max_distance = (goal_distance - added_distance) * (1 + tolerance)
-
-        waypoint_ids = [w["id"] for w in waypoints]
-        start_node = {"id": waypoint_ids[0], "coords": self.G.nodes[waypoint_ids[0]]["coords"]}
         completed = []
-        considering = deque([Route(nodes=[start_node], waypoints_hit=[start_node])])
+        considering = deque([Route(nodes=[self.waypoint_ids[0]], next_waypoint_idx=1)])
         while len(considering):
             route = considering.popleft()
             routes = self.extend_by_one_node(route)
             # Determine which routes will be enqueued, discarded, or added to completed
             for new_r in routes:
-                if new_r.nodes[-1]["id"] == waypoints[len(new_r.waypoints_hit)]["id"]:
+                if new_r.next_waypoint_idx is None:
                     # TODO stuff
                     pass
 
     def extend_by_one(self, route):
         routes = []
-        last_lat_lon = route.nodes[-1]["coords"][::-1]
-        for node, coords in self.neighbors(route.nodes[-1]):
-            # update distance
-            distance = route.distance + haversine(last_lat_lon, coords[::-1], unit=Unit.METERS)
-            # update waypoints_hit
+        last_lat_lon = self.lat_lon(route.nodes[-1])
+        for n in self.neighbors(route.nodes[-1]):
+            nodes = route.nodes + [n]
+            distance = route.distance + haversine(last_lat_lon, self.lat_lon(n), unit=Unit.METERS)
+            if n == self.waypoint_ids[route.next_waypoint_idx]:
+                next_waypoint_idx = (None if (route.next_waypoint_idx + 1 == len(self.waypoint_ids))
+                                     else (route.next_waypoint_idx + 1))
+            else:
+                next_waypoint_idx = route.next_waypoint_idx
+            entered_turn = route.entered_turn
+            n_turns = route.n_turns
+            if entered_turn and self.exiting_turn(nodes):
+                entered_turn = False
+                n_turns += 1
+            elif not entered_turn and self.entering_turn(nodes):
+                entered_turn = True
+            routes.append(nodes, distance, n_turns, entered_turn, next_waypoint_idx)
 
-            # update n_turns
+    def entering_turn(self, nodes) -> bool:
+        if len(nodes) < 3:
+            return False
+        points = [self.lat_lon(n) for n in reversed(nodes)]
+        angles = [surface_turn_angle(p1, p2, p3) for p1, p2, p3 in triplewise(points)]
+        distances = [haversine(p1, p2, unit=Unit.METERS) for p1, p2 in pairwise(points)]
+        distance_accum = distances[0]
+        angle_accum = 0
+        for d, a in zip(distances[1:], angles):
+            distance_accum += d
+            angle_accum += a
+            if distance_accum > self.turn_radius:
+                return False
+            elif abs(angle_accum) > self.turn_angle:
+                return True
+        return False
+
+    def exiting_turn(self, nodes) -> bool:
+        if len(nodes) < 3:
+            return False
+        points = [self.lat_lon(n) for n in reversed(nodes)]
+        angles = [surface_turn_angle(p1, p2, p3) for p1, p2, p3 in triplewise(points)]
+        distances = [haversine(p1, p2, unit=Unit.METERS) for p1, p2 in pairwise(points)]
+        distance_accum = distances[0]
+        angle_accum = 0
+        for d, a in zip(distances[1:], angles):
+            distance_accum += d
+            angle_accum += a
+            if abs(angle_accum) > self.turn_angle:
+                return False
+            elif distance_accum > self.turn_radius:
+                return True
+        return False
 
 
 class Route:
-    def __init__(self, nodes, distance=0, n_turns=0, waypoints_hit=None):
+    def __init__(self, nodes, distance=0, n_turns=0, entered_turn=False, next_waypoint_idx=None):
         self.nodes = nodes
         self.distance = distance
+        self.entered_turn = entered_turn
         self.n_turns = n_turns
-        self.waypoints_hit = waypoints_hit or []
+        self.next_waypoint_idx = next_waypoint_idx
--- osmthedistance/util.py
+++ osmthedistance/util.py
@@ -1,6 +1,8 @@
 from itertools import tee
+from math import radians, sqrt, sin, asin, degrees, atan, cos, atan2
 
 from haversine import haversine, Unit
+from haversine.haversine import get_avg_earth_radius
 from tqdm import tqdm
 import pydash as py_
 
@@ -10,6 +12,15 @@ def pairwise(iterable):
     a, b = tee(iterable)
     next(b, None)
     return zip(a, b)
+
+
+def triplewise(iterable):
+    """s -> (s0,s1,s2), (s1,s2,s3), (s2,s3,s4), ..."""
+    a, b, c = tee(iterable, n=3)
+    next(b, None)
+    next(c, None)
+    next(c, None)
+    return zip(a, b, c)
 
 
 def lat_lon(node):
@@ -61,3 +72,33 @@ def remdups(seq, key=id):
             rv.append(item)
             seen.add(k)
     return rv
+
+
+def surface_turn_angle(point1, point2, point3):
+    """Angle of turn going from point1 to point3 through point2."""
+    # unpack latitude/longitude
+    lat1, lng1 = point1
+    lat2, lng2 = point2
+    lat3, lng3 = point3
+
+    # convert all latitudes/longitudes from decimal degrees to radians
+    lat1, lng1 = radians(lat1), radians(lng1)
+    lat2, lng2 = radians(lat2), radians(lng2)
+    lat3, lng3 = radians(lat3), radians(lng3)
+
+    # arc lengths (angles in radians subtended from center of sphere) between points
+    a = sqrt((lat1 - lat2) ** 2 + (lng1 - lng2) ** 2)
+    b = sqrt((lat2 - lat3) ** 2 + (lng2 - lng3) ** 2)
+    c = sqrt((lat1 - lat3) ** 2 + (lng1 - lng3) ** 2)  # not actually traversed
+
+    # law of haversines to get angle C of spherical triangle.
+    angle = degrees(2 * asin(sqrt(
+        (sin(c * 0.5) ** 2 - sin((a - b) * 0.5) ** 2) /
+        (sin(a) * sin(b))
+    )))
+
+    # get direction of "out-of-plane" component of cross product => left or right turn
+    ai, aj = lng2 - lng1, lat2 - lat1
+    bi, bj = lng3 - lng2, lat3 - lat2
+    left_turn = ai * bj - aj * bi > 0
+    return (1 if left_turn else -1) * (180 - angle)
